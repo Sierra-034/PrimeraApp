57d265915a6dc34ba6c87e9b8b2134e123b4fa44
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2022-01-25 11:25:17 -0600

    70: Posición absoluta

--- src/screens/PositionScreen.tsx
+++ src/screens/PositionScreen.tsx
@@ -7,15 +7,18 @@ export const PositionScreen = () => {
         <View style={styles.container} >
             <View style={styles.cajaMorada} />
             <View style={styles.cajaNaranja} />
+            <View style={styles.cajaVerde} />
         </View>
     )
 };
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        // flex: 1,
+        width: 300,
+        height: 400,
+        // justifyContent: 'center',
+        // alignItems: 'center',
         backgroundColor: '#28C4D9',
     },
     cajaMorada: {
@@ -24,10 +27,9 @@ const styles = StyleSheet.create({
         backgroundColor: '#5856D6',
         borderWidth: 10,
         borderColor: 'white',
-        bottom: 50,
-        top: 50,
-        right: 50,
-        left: 50,
+        position: 'absolute',
+        top: 10,
+        right: 10,
     },
     cajaNaranja: {
         width: 100,
@@ -35,5 +37,20 @@ const styles = StyleSheet.create({
         backgroundColor: '#F0A23B',
         borderWidth: 10,
         borderColor: 'white',
+        position: 'absolute',
+        right: 10,
+        bottom: 10,
+    },
+    cajaVerde: {
+        width: 100,
+        // height: 100,
+        backgroundColor: 'green',
+        borderWidth: 10,
+        borderColor: 'white',
+        position: 'absolute',
+        top: 10,
+        left: 10,
+        bottom: 10,
+        right: 10,
     },
 });
