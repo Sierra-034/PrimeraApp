0d525f512bc18ccce7ec91d264e67bcd48b2d61f
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2022-01-27 11:00:20 -0600

    72: Flex

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import { PositionScreen } from './src/screens/PositionScreen';
+import { FlexScreen } from './src/screens/FlexScreen';
+// import { PositionScreen } from './src/screens/PositionScreen';
 // import { DimensionesScreen } from './src/screens/DimensionesScreen';
 // import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
 // import { ContadorScreen } from './src/screens/ContadorScreen';
@@ -14,7 +15,8 @@ export const App = () => {
       {/* <ContadorScreen /> */}
       {/* <BoxObjectModelScreen /> */}
       {/* <DimensionesScreen /> */}
-      <PositionScreen />
+      {/* <PositionScreen /> */}
+      <FlexScreen />
     </SafeAreaView>
   )
 };
